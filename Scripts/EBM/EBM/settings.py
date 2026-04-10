4065c629a5bc27ce7b05a32173622cfc21996345
artugal28373 <jucse28.373@gmail.com>
2026-04-11 03:46:08 +0600

    EBMS-9: Remove duplicate STATICFILES_DIRS and DEFAULT_AUTO_FIELD
    - Remove first duplicate STATICFILES_DIRS (lines 74-76)
    - Remove first duplicate DEFAULT_AUTO_FIELD (line 130)
    - Keep the properly commented versions

--- Scripts/EBM/EBM/settings.py
+++ Scripts/EBM/EBM/settings.py
@@ -71,9 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EBM.wsgi.application'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -127,7 +124,6 @@ STATIC_URL = 'static/'
 MEDIA_ROOT=BASE_DIR/ "media"
 MEDIA_URL="/media/"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
