3517d88f3cfcb910244d293681c0f8c4f7963b4f
artugal28373 <jucse28.373@gmail.com>
2026-04-11 09:09:22 +0600

    fix: Restore @login_required on home view

--- Scripts/EBM/root/views.py
+++ Scripts/EBM/root/views.py
@@ -16,6 +16,7 @@ from .services import BillCalculator, get_semester_display
 
 # Create your views here.
 
+@login_required(login_url='/log')
 def home(request):
     return render(request, 'home.html')
 
